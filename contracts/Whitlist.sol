7c24fe5b52327caf7ec725785c3069feffe13b97
Shriraj Pawar <68687869+shrirajpawar4@users.noreply.github.com>
2022-08-23 00:46:35 +0530

    first commit. Initial contract

---
+++ contracts/Whitlist.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    uint8 public maxWhiteListedAddress;
+
+    mapping (address => bool) public whiteListAddresses; //if address whitlisted, gets true. By default false for every other.
+
+    uint8 public numAddressesWhiteListed;
+
+    constructor (uint8 _maxWhiteListedAddress) {
+        maxWhiteListedAddress = _maxWhiteListedAddress;
+    }
+
+    //function to add address of sender to whitelist
+    function addAddressToWhiteList () public {
+        require(!whiteListAddresses[msg.sender], "Sender is already whitelisted");
+
+        require(numAddressesWhiteListed < maxWhiteListedAddress, "Limit reached, can't add more accounts");
+
+        whiteListAddresses[msg.sender] = true;
+        numAddressesWhiteListed += 1;
+    }
+}
